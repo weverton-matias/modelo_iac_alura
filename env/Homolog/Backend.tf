e802bb96d55bd9d9211ef464034590150d5ad3dc
Weverton Matias <weverton-matia@hotmail.com>
2025-07-06 16:49:54 -0300

    Ajustando parâmetros e fixando versão do terraform.

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-alura"
-    key    = "Prod/terraform.tfstate"
-    region = "us-west-2"
+    bucket = "formacoes-alura"
+    key    = "Homolog/terraform.tfstate"
+    region = "us-east-1"
   }
 }
--- env/Homolog/Main.tf
+++ env/Homolog/Main.tf
@@ -4,7 +4,7 @@ module "homolog" {
     nome_repositorio = "homolog"
     cargoIAM = "homolog"
     ambiante = "homolog"
-    imagem="leonardosartorello/go_ci:22"
+    imagem="wevertonmatias/go_ci:54"
 }
 
 output "IP_alb" {
